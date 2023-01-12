ef623528deee81cdf6e5751d02767f4bda79d32d
”Andres <Andres.Mamani@jala.university>
2023-01-12 11:29:04 -0400

    Base projet was added in repo

---
+++ BillingProyect/BillingSystem/Controller/BillingSystemApp.cs
@@ -0,0 +1,41 @@
+﻿using BillingSystem.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillingSystem.Controller
+{
+    public class BillingSystemApp
+    {
+        public List<Member> memberList = new();
+        public void RegisterMember(Member member)
+        {
+            if (CheckIfMemberExist(member.Id))
+            {
+                throw new Exception("The member already exists in the list.");
+            }
+            memberList.Add(member);
+        }
+
+        public void RegisterWaterConsumptionReading(int Id, string mes, int amount)
+        {
+            if (!CheckIfMemberExist(Id))
+            {
+                throw new Exception("The member does not exist in the list.");
+            }
+            int index = memberList.FindIndex(x => x.Id == Id);
+            memberList[index].AddConsumption(mes, amount);
+        }
+
+        public bool CheckIfMemberExist(int id)
+        {
+            return memberList.Any(x => x.Id == id);
+        }
+        public Member FindMemberById(int Id)
+        {
+            return memberList.Find(x => x.Id == Id) ?? throw new Exception($"A member with the specified ID: {Id} was not found.");
+        }
+    }
+}
---
+++ BillingProyect/BillingSystem/Model/Associate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillingSystem.Model
+{
+    public class Member : User
+    {
+        public List<Dictionary<string, int>> WaterConsumptionPerMonth = new List<Dictionary<string, int>>();
+
+
+        public void AddConsumption(string mes, int amount)
+        {
+            var consumption = new Dictionary<string, int>();
+            consumption.Add(mes, amount);
+            WaterConsumptionPerMonth.Add(consumption);
+
+
+        }
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name :{Name}, LastName: {Lastname}, Direction: {Direction}";
+        }
+
+        public void PrintWater()
+        {
+            for (int i = 0; i < WaterConsumptionPerMonth.Count; i++)
+            {
+                Console.WriteLine(WaterConsumptionPerMonth[i]);
+            }
+        }
+    }
+}

18b0d02dbd666fae65ee03ff1550e03f3e84b185
Michelle Cadavid <cadavidcarmona1@gmail.com>
2023-01-12 13:52:18 -0400

    program

--- BillingProyect/BillingSystem/Program.cs
+++ BillingProyect/BillingSystem/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BillingSystem.Controller;
+using BillingSystem.View;
+
+BillingSystemApp myBillingSys = new();
+MainMenu.Show(myBillingSys);

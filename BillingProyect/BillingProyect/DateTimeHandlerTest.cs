96ad35e6e63d18c825acc5c2245b4123594218fa
”Andres <Andres.Mamani@jala.university>
2023-01-12 13:42:07 -0400

    Update projectt

---
+++ BillingProyect/BillingProyect/DateTimeHandlerTest.cs
@@ -0,0 +1,26 @@
+﻿using BillingSystem.Helper;
+
+namespace BillingSystemTest
+{
+    [TestClass]
+    public class DatetimeHandlerTest
+    {
+        [TestMethod]
+        public void TestShouldConvertStringDateToDateTimeDate()
+        {
+            DateTime actual = DateTimeHandler.ConvertStringToDateTime("31/03/2017");
+            DateTime expected = DateTime.Parse("31/03/2017");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestConvertToString()
+        {
+            DateTime dateT = DateTime.Parse("31/03/2017");
+            var actual = DateTimeHandler.ConvertDateTimeToString(dateT);
+            var expected = "31/03/2017";
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
--- BillingProyect/BillingSystem/Model/WaterConsumption.cs
+++ BillingProyect/BillingSystem/Model/WaterConsumption.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BillingSystem.Helper;
 
 namespace BillingSystem.Model
 {
-    internal class WaterConsumption
+    public class WaterConsumption
     {
+        public DateTime DateTime { get; set; }
+        public int Amount { get; set; }
+
+        public void LoadAttributes(List<string> attributesList)
+        { 
+            if (!DateTimeHandler.CheckIfStringIsDateTime(attributesList[0])) // 10/10/2022
+            {
+                throw new Exception("Invalid Date.");
+            }
+            int number;
+            if (!int.TryParse(attributesList[1], out number))
+            {
+                throw new Exception("ERROR: please enter integers only.");
+            }
+            DateTime = DateTimeHandler.ConvertStringToDateTime(attributesList[0]);
+            Amount = int.Parse(attributesList[1]); ;
+        }
     }
 }
